269684f130d5b8c9a80f99e9b5739aa1e3db9904
unknown <juliansantanatoglia@gmail.com>
2024-08-30 23:51:10 +0200

    empezando cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 // IMPORTS
+
 import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { CartProvider } from './context/CartContext'
+import CartContextProvider from './context/CartContext'
 import Layout from './components/pages/Layout'
-import Contact from './components/Contact/Contact'
-import AboutUs from './components/AboutUs/AboutUs'
 
 
-// COMPONENTES
 function App() {
+  const TituloDelProyecto = "Camisetas de Futbol";
+  
   return (
+    <CartContextProvider>
     <div className="App">
     <BrowserRouter>
-    {/* <CartProvider> */}
     <Layout> 
       <Routes>
-        <Route path='/' element={<ItemListContainer />}/>
+        <Route path='/' element={<ItemListContainer title={TituloDelProyecto} />}/>
         <Route path='/home' element={<ItemListContainer />}/>
         <Route path='/camisetas/:categoryId' element={<ItemListContainer />}/>
         <Route path='/product/:id' element={<ItemDetailContainer />}/>
         <Route path='*' element={<h1 className="error-404">Lo siento, esta página no existe</h1>}/>
       </Routes>
     </Layout>
-    {/* </CartProvider> */}
     </BrowserRouter>
-  
-
     </div>
+    </CartContextProvider>
+
   );
 }
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,37 +1,70 @@
-import { createContext, useState } from 'react'
+import { useState, useEffect, createContext, useContext } from 'react'
 
-export const CartContext = createContext({
-    cart: []
-})
+const CartContext = createContext();
 
-export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([])
-    console.log(cart)
+export const { Provider } = CartContext;
 
-    const addItem = (item, quantity) => {
-        if(!isInCart(item.id)) {
-            setCart(prev => [...prev, {...item, quantity}])
-        } else {
-            console.error('El producto ya fue agregado')
-        }
-    }
+export const useCartContext = () => {
+    return useContext(cartContext)
+}
 
-    const removeItem = (itemId) => {
-        const cartUpdated = cart.filter(prod => prod.id !== itemId)
-        setCart(cartUpdated)
-    }
-
-    const clearCart = () => {
-        setCart([])
-    }
-
-    const isInCart = (itemId) => {
-        return cart.some(prod => prod.id === itemId)
+const CartContextProvider = ({children}) => {
+    
+    const contextValue = {
+        titulo: 'Aca va un titulo'
     }
 
     return (
-        <CartContext.Provider value={{cart, addItem, removeItem, clearCart}}>  
-            { children }
-        </CartContext.Provider>
+        <Provider value={contextValue}>
+        {children}
+        </Provider>
     )
 }
+
+
+export default CartContextProvider;
+
+
+
+
+
+
+
+
+
+
+// export const CartContext = createContext({
+//     cart: []
+// })
+
+// export const CartProvider = ({ children }) => {
+//     const [cart, setCart] = useState([])
+//     console.log(cart)
+
+//     const addItem = (item, quantity) => {
+//         if(!isInCart(item.id)) {
+//             setCart(prev => [...prev, {...item, quantity}])
+//         } else {
+//             console.error('El producto ya fue agregado')
+//         }
+//     }
+
+//     const removeItem = (itemId) => {
+//         const cartUpdated = cart.filter(prod => prod.id !== itemId)
+//         setCart(cartUpdated)
+//     }
+
+//     const clearCart = () => {
+//         setCart([])
+//     }
+
+//     const isInCart = (itemId) => {
+//         return cart.some(prod => prod.id === itemId)
+//     }
+
+//     return (
+//         <CartContext.Provider value={{cart, addItem, removeItem, clearCart}}>  
+//             { children }
+//         </CartContext.Provider>
+//     )
+// }
